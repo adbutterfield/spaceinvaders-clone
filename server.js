dd77ffe93517bdd777a0095c2ea352018d00fab8
Adam Butterfield <adb707@yahoo.com>
2014-11-18 14:33:09 -0800

    Add port for production to app.listen

--- server.js
+++ server.js
@@ -3,6 +3,4 @@ var path = require('path');
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('port', 3000);
-app.listen(app.get('port'), function(){
-  console.log("Listening on: " + app.get('port'));
-});
+app.listen(process.env.PORT || app.get('port'));
